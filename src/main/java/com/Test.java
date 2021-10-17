1f1597ee6ac1f6e111b16ed4a42c6b82b738ea7c
Maksym Phasolya <maksymphasolya@gmail.com>
2021-10-17 19:48:54 +0300

    add tests to services, javadoc, fix bags

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,7 +1,15 @@
 package com;
 
+
+import org.apache.log4j.Logger;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world!");
+
+        Logger logger = Logger.getLogger(Test.class);
+
+        logger.info("test");
+
     }
 }
